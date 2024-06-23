ec7ac832e9798af4841b0b60d65f13057be61471
Catchawink <threeeasywords@gmail.com>
2024-06-22 21:42:39 -0400

    Updated files.

--- src/web/utils.rs
+++ src/web/utils.rs
@@ -23,10 +23,10 @@ pub fn get_bluetooth_characteristic(device_id: Uuid, service_id: Uuid, character
 }
 
 pub async fn sleep(duration: Duration) {
-    let (response_tx, response_rx) = oneshot::channel::<()>();
+    //let (response_tx, response_rx) = oneshot::channel::<()>();
     
-    wasm_bindgen_futures::spawn_local(async move {
-        
+   // wasm_bindgen_futures::spawn_local(async move {
+        /*
             let mut cb = |resolve: js_sys::Function, _reject: js_sys::Function| {
                 web_sys::window()
                     .unwrap()
@@ -36,11 +36,11 @@ pub async fn sleep(duration: Duration) {
         
             let p = js_sys::Promise::new(&mut cb);
             wasm_bindgen_futures::JsFuture::from(p).await.unwrap();
-
-            //async_std::task::sleep(std::time::Duration::from_millis(duration.as_millis() as u64)).await;
+ */
+            async_std::task::sleep(std::time::Duration::from_millis(duration.as_millis() as u64)).await;
                 
-            response_tx.send(()).unwrap();
-        }
-    );
-    response_rx.await.unwrap();
+            //response_tx.send(()).unwrap();
+        //}
+    //);
+    //response_rx.await.unwrap();
 }
